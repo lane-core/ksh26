5c3d0a06ea6eb683611f4d16569211c7f2ac989d
Martijn Dekker <martijn@inlv.org>
2025-05-26 00:39:37 +0100

    Fix POSIX mode regression in test/[ -a/-o (re: 9b259c94)
    In ksh's POSIX mode, there is a bug as of that commit:
    "test ! foo -o bar" returns 0, must return 1.
    "test ! foo -a ''" returns 1, must return 0.
    
    This replace the old broken fix with two new fixes.
    
    src/cmd/ksh93/bltins/test.c:
    - e3(): Remove the old kludge.
    - b_test(): In POSIX mode, enforce a violation of basic logic that
      sadly made it to every other shell: "test ! foo -o bar" must
      return 1, and "test ! foo -a ''" must return 0. This only applies
      if argc==5, i.e., if there are 4 command arguments. It's done by
      skipping the '!' and returning the logical inverse of the
      expression without the '1'.
    - e3(): In POSIX mode, do not process ! or ( as operators if
      followed directly by -a or -o; this disables the nonstandard
      unary -a and -o operators in these cases, so the standard binary
      -a and -o operators can override them.
    
    (Yes, I'm aware POSIX-1.2024 removed -a and -o altogether, along
    with parentheses, and now only supports simple expressions for
    test. But we're still trying to be compatible with other shells and
    older POSIX versions as well.)

--- src/cmd/ksh93/data/testops.c
+++ src/cmd/ksh93/data/testops.c
@@ -2,7 +2,7 @@
 *                                                                      *
 *               This software is part of the ast package               *
 *          Copyright (c) 1982-2012 AT&T Intellectual Property          *
-*          Copyright (c) 2020-2024 Contributors to ksh 93u+m           *
+*          Copyright (c) 2020-2025 Contributors to ksh 93u+m           *
 *                      and is licensed under the                       *
 *                 Eclipse Public License, Version 2.0                  *
 *                                                                      *
@@ -53,7 +53,7 @@ const Shtable_t shtab_testops[] =
 };
 
 const char sh_opttest[] =
-"[-1c?\n@(#)$Id: test (ksh 93u+m) 2024-12-08 $\n]"
+"[-1c?\n@(#)$Id: test (ksh 93u+m) 2025-05-26 $\n]"
 "[--catalog?" SH_DICT "]"
 "[+NAME?test, [ - evaluate expression]"
 "[+DESCRIPTION?\btest\b evaluates expressions and returns its result using the "
