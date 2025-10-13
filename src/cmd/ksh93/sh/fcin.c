0f6866b6bebc26d003acb6f467d0694a8aea5177
Martijn Dekker <martijn@inlv.org>
2025-10-13 21:25:31 +0100

    [v1.1] ksh: fcin: remove _Fcin.fcoff, fctell() (re: f21ace5c)
    The offset field fcoff, and the associated fctell function macro,
    were only used to remember the offset to a function defintion in a
    script file before the referenced commit. Now that they are no
    longer used, they can be deleted.

--- src/cmd/ksh93/sh/fcin.c
+++ src/cmd/ksh93/sh/fcin.c
@@ -55,7 +55,6 @@ int	fcfopen(Sfio_t* f)
 	n = sfvalue(f);
 	fcrestore(&save);
 	sfread(f,buff,0);
-	_Fcin.fcoff = sftell(f);
 	buff = (char*)sfreserve(f,SFIO_UNBOUND,SFIO_LOCKR);
 	_Fcin.fclast = (_Fcin.fcptr=_Fcin.fcbuff=(unsigned char*)buff)+n;
 	if(sffileno(f) >= 0)
@@ -81,8 +80,6 @@ int	fcfill(void)
 		/* see whether pointer has passed null byte */
 		if(ptr>_Fcin.fcbuff && *--ptr==0)
 			_Fcin.fcptr=ptr;
-		else
-			_Fcin.fcoff = 0;
 		return 0;
 	}
 	if(last)
@@ -97,7 +94,6 @@ int	fcfill(void)
 	if((n = ptr-_Fcin.fcbuff) && _Fcin.fcfun)
 		(*_Fcin.fcfun)(f,(const char*)_Fcin.fcbuff,n,_Fcin.context);
 	sfread(f, (char*)_Fcin.fcbuff, n);
-	_Fcin.fcoff +=n;
 	_Fcin._fcfile = 0;
 	if(!last)
 		return 0;
